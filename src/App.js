4f103e52d152dc32b35aaf0d164f80b02433f816
Andy_Janisz <andy.janisz@gmail.com>
2022-02-26 15:06:17 +0100

    Added lazy loading and Loading Spinner

--- src/App.js
+++ src/App.js
@@ -1,34 +1,53 @@
+import React, { Suspense } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import HomePage from "./components/pages/HomePage";
-import SearchPage from "./components/pages/SearchPage";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
 import MealDetail from "./components/Meals/MealDetail";
-import SortByCategory from "./components/pages/ByCategoryPage";
-import ByIngridientPage from "./components/pages/ByIngridientPage";
+import NotAvailable from "./components/pages/404";
+
+const SearchPage = React.lazy(() => import("./components/pages/SearchPage"));
+const ByIngridientPage = React.lazy(() =>
+  import("./components/pages/ByIngridientPage")
+);
+const SortByCategory = React.lazy(() =>
+  import("./components/pages/ByCategoryPage")
+);
 
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/home" />
-        </Route>
-        <Route path="/home" exact>
-          <HomePage />
-        </Route>
-        <Route path="/meal/:id">
-          <MealDetail />
-        </Route>
-        <Route path="/categories/">
-          <SortByCategory />
-        </Route>
-        <Route path="/search/q=:param">
-          <SearchPage />
-        </Route>
-        <Route path="/ingredients/">
-          <ByIngridientPage />
-        </Route>
-      </Switch>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/home" />
+          </Route>
+          <Route path="/home" exact>
+            <HomePage />
+          </Route>
+          <Route path="/meal/:id">
+            <MealDetail />
+          </Route>
+          <Route path="/categories/">
+            <SortByCategory />
+          </Route>
+          <Route path="/search/q=:param">
+            <SearchPage />
+          </Route>
+          <Route path="/ingredients/">
+            <ByIngridientPage />
+          </Route>
+          <Route path="*">
+            <NotAvailable />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
---
+++ src/components/pages/404.js
@@ -0,0 +1,4 @@
+const NotAvailable = () => {
+  return <div> Ups, where did you go? That page is not available! </div>;
+};
+export default NotAvailable;
